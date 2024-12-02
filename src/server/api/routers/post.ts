6b2c7b378ad3845685b661688be6798687cb1e0c
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-12-02 23:40:41 +0100

    configuration, ignore

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -11,15 +11,15 @@ export const postRouter = createTRPCRouter({
       };
     }),
 
-  // create: publicProcedure
-  //   .input(z.object({ name: z.string().min(1) }))
-  //   .mutation(async ({ ctx, input }) => {
-  //     return ctx.db.post.create({
-  //       data: {
-  //         name: input.name,
-  //       },
-  //     });
-  //   }),
+  create: publicProcedure
+    .input(z.object({ name: z.string().min(1) }))
+    .mutation(async ({ ctx, input }) => {
+      return ctx.db.post.create({
+        data: {
+          name: input.name,
+        },
+      });
+    }),
 
   getLatest: publicProcedure.query(async ({ ctx }) => {
     const post = await ctx.db.post.findFirst({
